3abb7b6b20d33c9289402d396f0073539116edd7
earljohn004 <earl.abaquita@gmail.com>
2024-10-06 20:12:35 +0800

    working join game

--- src/app/games/create/page.tsx
+++ src/app/games/create/page.tsx
@@ -1,38 +1,116 @@
 "use client";
 
 import { Box, MenuItem, TextField } from "@mui/material";
+import { CreateResponse, useCreate, useSelect } from "@refinedev/core";
 import { Create } from "@refinedev/mui";
 import { useForm } from "@refinedev/react-hook-form";
+import { Controller } from "react-hook-form";
+
+interface PlayerInformation {
+  first_name: string;
+  last_name: string;
+  player_id: string;
+}
+
+interface CreateGameReturn {
+  data: {
+    game_id: string;
+    game_mode: string;
+    game_race: number;
+    created_at: Date;
+  };
+}
 
 const GamesCreate = () => {
   const {
+    saveButtonProps: gamePlayer1Mutate,
+    setValue: gamePlayer1SetValue,
+    control: gamePlayer1Control,
+  } = useForm({ refineCoreProps: { resource: "game_players" } });
+
+  const {
+    saveButtonProps: gamePlayer2Mutate,
+    setValue: gamePlayer2SetValue,
+    control: gamePlayer2Control,
+  } = useForm({ refineCoreProps: { resource: "game_players" } });
+
+  const {
     saveButtonProps,
-    refineCore: { formLoading },
+    refineCore: { formLoading: gameInformationFormLoading },
     register,
-    formState: { errors },
-  } = useForm({ refineCoreProps: { resource: "game_information" } });
+  } = useForm({
+    refineCoreProps: {
+      resource: "game_information",
+      onMutationSuccess: (data: unknown) => {
+        const gameData = data as CreateGameReturn;
+
+        // Store player id as player 1 on game_players table
+        gamePlayer1SetValue("game_id", gameData.data.game_id);
+        gamePlayer1Mutate.onClick("" as any);
+
+        // Store player id as player 2 on game_players table
+        gamePlayer2SetValue("game_id", gameData.data.game_id);
+        gamePlayer2Mutate.onClick("" as any);
+      },
+    },
+  });
+
+  const { options } = useSelect<PlayerInformation>({
+    resource: "player_information",
+    optionLabel: (item) => `${item.first_name} ${item.last_name}`,
+    optionValue: "player_id",
+  });
 
   return (
-    <Create isLoading={formLoading} saveButtonProps={saveButtonProps}>
+    <Create
+      isLoading={gameInformationFormLoading}
+      saveButtonProps={saveButtonProps}
+    >
       <Box
         component="form"
         sx={{ display: "flex", flexDirection: "column" }}
         autoComplete="off"
       >
-        <TextField
-          margin="normal"
-          fullWidth
-          InputLabelProps={{ shrink: true }}
-          type="text"
-          label="Player 1"
+        <Controller
+          name="player_id"
+          control={gamePlayer1Control}
+          render={({ field }) => (
+            <TextField
+              select
+              label="Player 1"
+              {...field}
+              margin="normal"
+              fullWidth
+            >
+              {options?.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
         />
-        <TextField
-          margin="normal"
-          fullWidth
-          InputLabelProps={{ shrink: true }}
-          type="text"
-          label="Player 2"
+
+        <Controller
+          name="player_id"
+          control={gamePlayer2Control}
+          render={({ field }) => (
+            <TextField
+              select
+              label="Player 2"
+              {...field}
+              margin="normal"
+              fullWidth
+            >
+              {options?.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          )}
         />
+
         <TextField
           select
           label="Game Type"
